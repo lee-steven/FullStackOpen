946a77756405074c6001b62019dba335d073758d
Steven Lee <lee.steven@wustl.edu>
2020-05-26 19:19:31 -0700

    Part 4b async/await only no tests

--- part4-TestingAndAdministration/blog-backend/controllers/blogs.js
+++ part4-TestingAndAdministration/blog-backend/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
+// Returns all the blogs in database
 blogRouter.get('/', (request, response) => {
     Blog.find({})
       .then(blogs => {
@@ -8,6 +9,7 @@ blogRouter.get('/', (request, response) => {
       })
 })
   
+// Adding a blog to database
 blogRouter.post('/', (request, response) => {
 const blog = new Blog(request.body)
 
@@ -15,6 +17,31 @@ const blog = new Blog(request.body)
         .then(result => {
         response.status(201).json(result)
         })
+})
+
+// Deletes a blog in database
+blogRouter.delete('/:id', async (request, response) => {
+  try{
+    await Blog.findByIdAndRemove(request.params.id)    
+    response.status(204).end()
+  } catch(exception){
+    next(exception)
+  }
+})
+
+// Updates number of likes for specific blog
+blogRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+  const blog = {
+    likes: body.likes
+  }
+  console.log(blog)
+  try{
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+    response.json(updatedBlog.toJSON())
+  } catch(exception) {
+    next(exception)
+  }
 })
 
 module.exports = blogRouter
